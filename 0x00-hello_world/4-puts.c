bc0a6089cb1c7507f3e1b4bb5e2ed70592b1c2e6
Chibuike Precious <ekeomacreative@gmail.com>
2022-12-09 06:36:05 -0800

    C Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 #include <stdio.h>
-
+/**
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
 {	
-	puts(Programming is like building a multilingual puzzle\n);
+	puts("\"Programming is like building a multilingual puzzle);
 	return(0);
 
 }
