66bcf264f6a7a654dbdd1681021a41b725bcbfa3
Chibuike Precious <ekeomacreative@gmail.com>
2022-12-09 07:25:13 -0800

    C Programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  * main - Entry
  * Return: Always 0
  */
+
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return(0);
 }
