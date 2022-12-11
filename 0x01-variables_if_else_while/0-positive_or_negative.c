4a547b86138f0d69f49f9a373f4e34eb7cc900d6
Chibuike Precious <ekeomacreative@gmail.com>
2022-12-10 20:45:31 -0800

    if, esle if, else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,23 +3,20 @@
 #include <time.h>
 /**
  * main - entry point
- * description - prints if a number is positive or negative
- * return: 0
+ * Description - prints if a number is positive or negative
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	
 		printf("%d is positive\n", n);
-	
 	else if (n == 0)
 		pritnf("%i is zero\n", n);
 	else
 		printf("%i is negative\n", n);
 	return (0);
-	
-
 }
