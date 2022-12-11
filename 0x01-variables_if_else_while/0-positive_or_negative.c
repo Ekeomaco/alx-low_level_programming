6580d8a087af78d0d619fa9469dfed11b1db975a
Chibuike Precious <ekeomacreative@gmail.com>
2022-12-11 05:35:13 -0800

    if, if else, else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,13 @@
-#!/bin/bash
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry
  * print the value of n status; zero, positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int n;
