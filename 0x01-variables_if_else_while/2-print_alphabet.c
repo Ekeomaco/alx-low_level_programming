4fe821f912d5f7cc84cdef482dad6b505f26d28e
Chibuike Precious <ekeomacreative@gmail.com>
2022-12-11 07:07:20 -0800

    Variables, if, else if, else

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,23 @@
-#!include <stdlib.h>
-#!include <time.h>
-#!include <stdio.h>
+#include <stdio.h>
+
 /**
- * it will randomly assign number to the 
- * variable n each is executed*/
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	int n;
+	char ch = 'a';
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (digit > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
-	else if (digit == 0)
-		printf("Last digit of %i is %i and is 0\n", n, digit);
-	else if (digit < 6 && digit ! = 0)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+
 	return (0);
 }
