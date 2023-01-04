7c9c90c3aa9003137b76704ca87e44638350f4dd
Chibuike Precious <ekeomacreative@gmail.com>
2023-01-04 08:05:31 -0800

    C - Recursion fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,16 @@
 include "main.h"
+
 /**
- * _print_rev_recursion - print a string in reverse
+ * _print_rev_recursion - prints a string in reverse
+ *
  * @s: string
- * Return 0;
+ *
+ * Return: nothing
 */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
