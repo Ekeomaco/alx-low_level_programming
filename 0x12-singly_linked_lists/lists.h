dc28978aac2612bbd6e1c7ee7383286f43c8131e
Chibuike Precious <ekeomacreative@gmail.com>
2023-01-26 13:44:10 -0800

    Fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+ifndef LIST_H
+#define LIST_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -10,7 +13,7 @@
  *
  * Description: singly linked list node structure
  * for Holberton project
- */
+*/
 typedef struct list_s
 {
 	char *str;
@@ -18,10 +21,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* LIST_H */
